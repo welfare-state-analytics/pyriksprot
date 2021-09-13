6da9e07b1840e22c1035ea95479210dce02417c8
Roger Mähler <roger.mahler@hotmail.com>
2021-09-13 15:18:54 +0200

    Fixed LICENSE

--- pyriksprot/tokenize.py
+++ pyriksprot/tokenize.py
@@ -1,10 +1,30 @@
 """
-Temporarily inlined code from Sparv v4.0.0.
-Sparv dependency is made optional (lacks Stanza v1.6 support which has improved performance)
+The MIT License (MIT)
 
-FIXME: Add Sparv LICENSE statement!!!
+Copyright (c) 2012-2018 Språkbanken, University of Gothenburg
 
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 """
+
+# NOTE: Temporarily inlined code from Sparv v4.0.0.
+#        Sparv dependency is made optional (lacks Stanza v1.6 support which has improved performance)
+
 import os
 import re
 from os.path import join as jj
