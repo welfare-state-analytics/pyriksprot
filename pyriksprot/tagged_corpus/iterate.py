a3bbb21447a7534ca7c6de4b6a7871d46cbb3542
Roger Mähler <roger.mahler@hotmail.com>
2021-12-10 12:16:34 +0100

    Handle empty protocols

--- pyriksprot/tagged_corpus/iterate.py
+++ pyriksprot/tagged_corpus/iterate.py
@@ -2,13 +2,17 @@ from __future__ import annotations
 
 from typing import Iterable, List, Tuple
 
-from ..interface import ProtocolSegment, ProtocolSegmentIterator
+from ..interface import Protocol, ProtocolSegment, ProtocolSegmentIterator
 from . import persist
 
 
 def multiprocessing_load(args) -> Iterable[ProtocolSegment]:
-    return persist.load_protocol(filename=args[0]).to_segments(
-        content_type=args[1], segment_level=args[2], segment_skip_size=args[3]
+
+    protocol: Protocol = persist.load_protocol(filename=args[0])
+    return (
+        []
+        if protocol is None
+        else protocol.to_segments(content_type=args[1], segment_level=args[2], segment_skip_size=args[3])
     )
 
 
@@ -16,10 +20,17 @@ class ProtocolIterator(ProtocolSegmentIterator):
     """Reads xml files using Protocol entity and returns a stream of `ProtocolSegment`"""
 
     def load(self, filename: str) -> List[Tuple[str, str, int]]:
-        return persist.load_protocol(filename=filename).to_segments(
-            content_type=self.content_type,
-            segment_level=self.segment_level,
-            segment_skip_size=self.segment_skip_size,
+
+        protocol: Protocol = persist.load_protocol(filename=filename)
+
+        return (
+            []
+            if protocol is None
+            else protocol.to_segments(
+                content_type=self.content_type,
+                segment_level=self.segment_level,
+                segment_skip_size=self.segment_skip_size,
+            )
         )
 
     def map_futures(self, imap, args):
