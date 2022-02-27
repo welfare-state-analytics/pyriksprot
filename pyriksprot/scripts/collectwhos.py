4ecd8468365f3d6427145974d8a4dc7335a765f4
Roger Mähler <roger.mahler@hotmail.com>
2022-02-27 09:11:22 +0100

    Simplified logic

--- pyriksprot/scripts/collectwhos.py
+++ pyriksprot/scripts/collectwhos.py
@@ -1,4 +1,3 @@
-import os
 import sys
 
 import click
@@ -8,23 +7,11 @@ from pyriksprot import metadata as md
 
 @click.command()
 @click.argument('corpus_folder', type=click.STRING)
-@click.argument('documents_filename', type=click.STRING)
-@click.argument('utterances_filename', type=click.STRING)
-@click.option('--force', type=click.BOOL, is_flag=True, help='Force overwrite', default=False)
-def main(corpus_folder: str, documents_filename: str, utterances_filename: str, force: bool = False) -> None:
+@click.argument('target_folder', type=click.STRING)
+def main(corpus_folder: str, target_folder: str) -> None:
     try:
-        documents, utterances = md.generate_utterance_index(
-            corpus_folder=corpus_folder,
-        )
 
-        for filename in [documents_filename, utterances_filename]:
-            if os.path.isfile(filename):
-                if not force:
-                    raise ValueError("file exists, use --force to overwrite")
-                os.unlink(filename)
-
-        documents.to_csv(documents_filename, sep="\t")
-        utterances.to_csv(utterances_filename, sep="\t")
+        md.generate_utterance_index(corpus_folder=corpus_folder, target_folder=target_folder)
 
     except Exception as ex:
         click.echo(ex)
@@ -32,4 +19,4 @@ def main(corpus_folder: str, documents_filename: str, utterances_filename: str, 
 
 
 if __name__ == "__main__":
-    main()
+    main()  # pylint: disable=no-value-for-parameter
