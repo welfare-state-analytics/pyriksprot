0a23000d9f5fb30747cc92177e67afe4b35d9bee
Roger Mähler <roger.mahler@hotmail.com>
2022-03-07 08:56:00 +0100

    Completed implementation

--- pyriksprot/metadata/person.py
+++ pyriksprot/metadata/person.py
@@ -1,14 +1,14 @@
 from __future__ import annotations
 
 import sqlite3
-from dataclasses import dataclass
+from dataclasses import dataclass, field, fields
 from functools import cached_property
-from typing import List, Mapping
+from loguru import logger
 
-import numpy as np
 import pandas as pd
 
 from . import codecs
+from . import utterance
 from . import utility as mdu
 
 DATA_TABLES: dict[str, str] = {
@@ -22,6 +22,26 @@ DATA_TABLES: dict[str, str] = {
 
 
 @dataclass
+class TermOfOffice:
+    # pid: int
+    # person_id: str
+    office_type_id: str
+    sub_office_type_id: int
+    # district_id: int
+    start_year: int
+    end_year: int
+    # start_date: str
+    # end_date: str
+
+
+@dataclass
+class PersonParty:
+    party_id: str
+    start_year: int
+    end_year: int
+
+
+@dataclass
 class Person:
     # pid: int
     person_id: str
@@ -30,43 +50,100 @@ class Person:
     party_id: int
     year_of_birth: int
     year_of_death: int
+    terms_of_office: list[TermOfOffice] = field(default_factory=list)
+
+    """Alt. parties: only used of person has multiple parties"""
+    alt_parties: list[TermOfOffice] = field(default_factory=list)
+
+    def party_at(self, year: int) -> int:
+        if self.party_id:
+            return self.party_id
+        if self.alt_parties and not self.is_unknown:
+            for x in [p for p in self.alt_parties if p.start_year > 0 and p.end_year > 0]:
+                if x.start_year <= year <= x.end_year:
+                    return x
+            for x in [p for p in self.alt_parties if p.start_year == 0]:
+                if x.start_year <= year <= (x.end_year or 9999):
+                    return x
+            """Questionable rule"""
+            return self.alt_parties[-1]
+        return 0
+
+    def term_of_office_at(self, year: int) -> TermOfOffice:
+        if self.terms_of_office and not self.is_unknown:
+            for o in self.terms_of_office:
+                if o.start_year <= year <= (o.end_year or 9999):
+                    return o
+            """Questionable rule"""
+            return self.terms_of_office[-1]
+        return TermOfOffice(office_type_id=0, sub_office_type_id=0, start_year=year, end_year=year)
+
+    @property
+    def is_unknown(self) -> bool:
+        return self.person_id == 'unknown'
 
 
 @dataclass
 class SpeakerInfo:
     person_id: str
     name: str
-    start_year: int = None
-    end_year: int = None
-    gender_id: int = 0
-    party_id: int = 0
-    office_type_id: int = None
-    sub_office_type_id: int = None
-    year_of_birth: int = None
-    year_of_death: int = None
-    chamber_id: int = None
+    gender_id: int
+    party_id: int
+    start_year: int
+    end_year: int
+    office_type_id: int
+    sub_office_type_id: int
+    # year_of_birth: int = None
+    # year_of_death: int = None
+    # chamber_id: int = None
     # district: str = None
     # occupation: str = None
     # title: str = None
     property_bag: dict = None
 
 
+def swap_rows(df: pd.DataFrame, i: int, j: int):
+    row_i, row_j = df.iloc[i].copy(), df.iloc[j].copy()
+    df.iloc[j], df.iloc[i] = row_i, row_j
+
+
+def index_of_person_id(df: pd.DataFrame, person_id: str) -> int:
+    return df.index[df['person_id'] == person_id].tolist()[0]
+
+
 # pylint: disable=too-many-public-methods
 class PersonIndex:
-    def __init__(self, data: dict, code_maps: codecs.Codecs):
-        self.data: dict = data
-        self.code_maps: codecs.Codecs = code_maps
+    def __init__(self, database_filename: str):
+        self.database_filename: str = database_filename
+        self.data: dict = None
 
-    @staticmethod
-    def load(database_filename: str) -> PersonIndex:
-        with sqlite3.connect(database=database_filename) as db:
+    def load(self) -> PersonIndex:
+        with sqlite3.connect(database=self.database_filename) as db:
+            self.data = mdu.load_tables(DATA_TABLES, db=db)
+            self.data['persons_of_interest'].rename_axis("pid", inplace=True)
+            """ ensure `unknown` has pid = 0 """
+            if self.persons.loc[0]['person_id'] != 'unknown':
+                swap_rows(self.persons, 0, index_of_person_id(self.persons, 'unknown'))
+        return self
 
-            data = mdu.load_tables(DATA_TABLES, db=db, types=mdu.COLUMN_TYPES, defaults=mdu.COLUMN_DEFAULTS)
-            data['persons_of_interest'].rename_axis("pid", inplace=True)
-            code_maps: codecs.Codecs = codecs.Codecs.load(source=db, persons=data.get("persons_of_interest"))
+    @cached_property
+    def lookups(self) -> codecs.Codecs:
+        data: codecs.Codecs = codecs.Codecs().load(source=self.database_filename)
+        if self.data:
+            data.extra_codecs = self.codecs
+        return data
 
-            riksprot_metadata: PersonIndex = PersonIndex(data, code_maps)
-            return riksprot_metadata
+    @cached_property
+    def pid2person_id(self) -> dict:
+        return self.persons['person_id'].to_dict()
+
+    @cached_property
+    def person_id2pid(self) -> dict:
+        return mdu.revdict(self.pid2person_id)
+
+    @cached_property
+    def pid2person_name(self) -> dict:
+        return self.persons['name'].to_dict()
 
     @property
     def persons(self) -> pd.DataFrame:
@@ -76,40 +153,61 @@ class PersonIndex:
     def terms_of_office(self) -> pd.DataFrame:
         return self.data['terms_of_office']
 
+    @cached_property
+    def person_lookup(self) -> dict[str, Person]:
+        return {person_id: self.get_person(person_id) for person_id in self.person_id2pid}
+
+    def get_person(self, person_id: str | int) -> Person:
+        try:
+            pid: int = person_id if isinstance(person_id, int) else self.person_id2pid.get(person_id)
+            data: dict = self.persons.loc[pid].to_dict()
+            terms: list[TermOfOffice] = self.terms_of_office_lookup.get(data['person_id'])
+            alt_parties: list[PersonParty] = self.person_multiple_party_lookup.get(data['person_id'])
+            return Person(terms_of_office=terms, alt_parties=alt_parties, **data)
+        except Exception as ex:
+            logger.info(f"{type(ex).__name__}: {ex}")
+            raise
+
+    def __getitem__(self, key: int | str) -> Person:
+        person_id: str = key if isinstance(key, str) else self.pid2person_id.get(key)
+        return self.person_lookup.get(person_id)
+
+    def __contains__(self, key: int | str) -> bool:
+        if isinstance(key, int):
+            return key in self.pid2person_id
+        return key in self.person_id2pid
+
+    def __len__(self) -> int:
+        return len(self.person_id2pid)
+
+    @cached_property
+    def terms_of_office_lookup(self) -> dict:
+        """Builds a person_id to person's terms-of-offices dict """
+        lookup: dict = mdu.group_to_list_of_records(
+            df=self.terms_of_office,
+            key='person_id',
+            properties=[f.name for f in fields(TermOfOffice)],
+            ctor=TermOfOffice,
+        )
+        return lookup
+
+    @cached_property
+    def person_multiple_party_lookup(self) -> dict:
+        """Builds a person_id to person's parties lookup, only for person """
+        lookup: dict = mdu.group_to_list_of_records(
+            df=self.person_multiple_party,
+            key='person_id',
+            properties=[f.name for f in fields(PersonParty)],
+            ctor=PersonParty,
+        )
+        return lookup
+
     @property
     def person_multiple_party(self) -> pd.DataFrame:
         return self.data['person_multiple_party']
 
-    @property
-    def person_yearly_party(self) -> pd.DataFrame:
-        return self.data['person_yearly_party']
-
-    def get_speaker_info(self, person_id: str, protocol_id: int, u_id: str) -> dict[str, SpeakerInfo]:
-        person = self.get_person(person_id)
-        gender_id: int = person.gender_id if person.gender_id else self.unknown_utterance_genders.get(u_id, 0)
-        party_id: int = 0
-        # protocol: self.protcols
-        speaker_info: SpeakerInfo = SpeakerInfo(
-            person_id=person.person_id,
-            name=person.name,
-            gender_id=gender_id,
-            party_id=party_id,
-        )
-        return speaker_info
-
-    def get_person(self, x) -> Person:
-        raise NotImplementedError()
-
-    @cached_property
-    def unknown_utterance_genders(self) -> dict[str, int]:
-        return self.data['unknown_utterance_gender']['gender_id'].to_dict()
-
-    @cached_property
-    def unknown_utterance_parties(self) -> dict[str, int]:
-        return self.data['unknown_utterance_party']['party_id'].to_dict()
-
     def overload_by_person(
-        self, df: pd.DataFrame, *, encoded: bool = True, drop: bool = True, columns: List[str] = None
+        self, df: pd.DataFrame, *, encoded: bool = True, drop: bool = True, columns: list[str] = None
     ) -> pd.DataFrame:
 
         columns: list = ['person_id', 'gender_id', 'party_id']
@@ -120,82 +218,71 @@ class PersonIndex:
         xi: pd.DataFrame = df.merge(self.persons[columns], right_index=True, how='left', **join_criterias)
 
         if not encoded and drop:
-            xi = self.code_maps.decode(xi, drop=True)
+            xi = self.lookups.decode(xi, drop=True)
 
         mdu.slim_table_types(xi, defaults=mdu.COLUMN_DEFAULTS, types=mdu.COLUMN_TYPES)
 
         return xi
 
-    #     def __getitem__(self, key) -> ParliamentaryRole:
+    @cached_property
+    def codecs(self) -> list[codecs.Codec]:
+        return [
+            codecs.Codec('decode', 'pid', 'person_id', self.pid2person_id.get),
+            codecs.Codec('encode', 'person_id', 'pid', self.person_id2pid.get),
+            codecs.Codec('encode', 'who', 'pid', self.person_id2pid.get),
+        ]
 
-    #         if key is None:
-    #             return None
+    @cached_property
+    def property_values_specs(self) -> list[dict[str, str | dict[str, int]]]:
+        return self.lookups.property_values_specs + [
+            dict(text_name='person_id', id_name='pid', values=self.person_id2pid),
+        ]
 
-    #         if key not in self.id2person:
-    #             logger.warning(f"ID `{key}` not found in parliamentary member/minister index")
-    #             self.id2person[key] = self.create_unknown(key=key)
-
-    #         return self.id2person[key]
-
-    #     def __contains__(self, key) -> bool:
-    #         return key in self.id2person
-
-    #     def __len__(self) -> int:
-    #         return len(self.id2person)
-
-    def get_speaker(self):
-
-        return None
-
-
-class MemberNotFoundError(ValueError):
-    ...
-
-
-# class ProtoMetaData:
+    def unknown_person(self) -> Person:
+        return Person(
+            person_id='unknown',
+            name='unknown',
+            gender_id=0,
+            party_id=0,
+            year_of_birth=0,
+            year_of_death=0,
+            terms_of_office=[],
+        )
 
 
-#     def __init__(self, *, members: pd.DataFrame, verbose: bool = False):
+class SpeekerInfoService:
+    def __init__(self, database_filename: str):
+        self.database_filename: str = database_filename
 
-#         self.members: pd.DataFrame = members if isinstance(members, pd.DataFrame) else self.load_members(members)
-#         self.members['party_abbrev'] = self.members['party_abbrev'].fillna('unknown')
+    @cached_property
+    def utterance_lookup(self) -> utterance.UtteranceLookup:
+        return utterance.UtteranceLookup().load(source=self.database_filename)
 
-#         if verbose:
-#             logger.info(f"size of mop's metadata: {size_of(self.members, 'MB', total=True)}")
+    @cached_property
+    def person_index(self) -> PersonIndex:
+        return PersonIndex(self.database_filename).load()
 
-#     def map_id2text_names(self, id_names: List[str]) -> List[str]:
-#         return [MEMBER_IDNAME2NAME_MAPPING.get(x) for x in id_names]
+    def get_speaker_info(self, protocol_id: int, u_id: str, person_id: str) -> dict[str, SpeakerInfo]:
 
-#     def get_member(self, who: str) -> dict:
-#         try:
-#             return self.members.loc[who].to_dict()
-#         except:
-#             MemberNotFoundError(f"ID={who}")
+        person = self.person_index[person_id]
+        gender_id: int = person.gender_id
+        party_id: int = person.party_id
+        year: int = self.utterance_lookup.protocols_lookup(protocol_id)['year']
+        if person.is_unknown:
+            gender_id = gender_id or self.utterance_lookup.unknown_gender_lookup.get(u_id, 0)
+            party_id = party_id or self.utterance_lookup.unknown_party_lookup.get(u_id, 0)
+        elif not party_id:
+            party_id = person.party_at(year)
 
-
-# def unknown_member() -> dict:
-#     return dict(
-#         id='unknown',
-#         role_type='unknown',
-#         born=0,
-#         chamber=np.nan,
-#         district=np.nan,
-#         start=0,
-#         end=0,
-#         gender='unknown',
-#         name='unknown',
-#         occupation='unknown',
-#         party='unknown',
-#         party_abbrev='unknown',
-#     )
-
-
-def as_slim_types(df: pd.DataFrame, columns: List[str], dtype: np.dtype) -> pd.DataFrame:
-    if df is None:
-        return None
-    if isinstance(columns, str):
-        columns = [columns]
-    for column in columns:
-        if column in df.columns:
-            df[column] = df[column].fillna(0).astype(dtype)
-    return df
+        term_of_office: TermOfOffice = person.term_of_office_at(year)
+        speaker_info: SpeakerInfo = SpeakerInfo(
+            person_id=person.person_id,
+            name=person.name,
+            gender_id=gender_id,
+            party_id=party_id,
+            office_type_id=term_of_office.office_type_id,
+            sub_office_type_id=term_of_office.sub_office_type_id,
+            start_year=term_of_office.start_year,
+            end_year=term_of_office.end_year,
+        )
+        return speaker_info
--- tests/metadata/metadata_index_test.py
+++ tests/metadata/metadata_index_test.py
@@ -1,16 +1,139 @@
 from pyriksprot import metadata as md
+from pyriksprot.metadata.person import index_of_person_id, swap_rows
+
+import pandas as pd
+import pytest
+
+DATABASE_FILENAME: str = "./metadata/riksprot_metadata.v0.4.0.db"
 
 
-def test_load_code_maps():
-    database_filename: str = "./metadata/riksprot_metadata.v0.4.0.db"
-    code_maps: md.MetaDataCodeMaps = md.MetaDataCodeMaps.load(database_filename, persons=None)
+@pytest.fixture
+def person_index() -> md.PersonIndex:
+    return md.PersonIndex(DATABASE_FILENAME).load()
 
-    assert code_maps is not None
+
+def test_code_lookups():
+    lookups: md.Codecs = md.Codecs().load(DATABASE_FILENAME)
+    assert lookups
+
+    assert lookups.gender2id.get("woman") == 2
+    assert lookups.gender2name.get(2) == "woman"
+
+    assert lookups.office_type2id.get("unknown") == 0
+    assert lookups.office_type2id.get("Minister") == 2
+    assert lookups.office_type2name.get(2) == "Minister"
+
+    assert lookups.sub_office_type2name.get(0) == "unknown"
+    assert lookups.sub_office_type2name.get(1) == "Ledamot av första kammaren"
+    assert lookups.sub_office_type2id.get("Ledamot av första kammaren") == 1
+
+    assert lookups.party_abbrev2name.get(0) == "?"
+
+    df: pd.DataFrame = pd.DataFrame(
+        data=dict(
+            gender_id=[0, 1, 2],
+            party_id=[0, 5, 3],
+            office_type_id=[0, 2, 3],
+            sub_office_type_id=[0, 2, 8],
+        )
+    )
 
 
 def test_load():
 
     database_filename: str = "./metadata/riksprot_metadata.v0.4.0.db"
-    metadata_index: md.PersonIndex = md.PersonIndex.load(database_filename)
+    person_index: md.PersonIndex = md.PersonIndex(database_filename).load()
 
-    assert metadata_index is not None
+    assert person_index is not None
+
+    expected_data: dict = {
+        'person_id': 'Q3358763',
+        'name': 'Monica Green',
+        'gender_id': 2,
+        'party_id': 8,
+        'year_of_birth': 1959,
+        'year_of_death': 0,
+    }
+
+    person: md.Person = person_index.get_person(500)
+    assert person is not None
+    assert expected_data == {k: v for k, v in person.__dict__.items() if k in expected_data}
+
+    person: md.Person = person_index.get_person('Q3358763')
+    assert person is not None
+    assert expected_data == {k: v for k, v in person.__dict__.items() if k in expected_data}
+
+    person_lookup = person_index.person_lookup
+    assert person_lookup is not None
+    person: md.Person = person_lookup['Q3358763']
+    assert expected_data == {k: v for k, v in person.__dict__.items() if k in expected_data}
+
+    terms: list[md.TermOfOffice] = person_index.terms_of_office_lookup.get('Q3358763')
+
+    assert (
+        terms
+        == person.terms_of_office
+        == [
+            md.TermOfOffice(office_type_id=1, sub_office_type_id=4, **x)
+            for x in [
+                {'start_year': 1994, 'end_year': 1998},
+                {'start_year': 1998, 'end_year': 2002},
+                {'start_year': 2002, 'end_year': 2006},
+                {'start_year': 2006, 'end_year': 2010},
+                {'start_year': 2010, 'end_year': 2014},
+                {'start_year': 2014, 'end_year': 2018},
+            ]
+        ]
+    )
+
+    Q6093334_pid = index_of_person_id(person_index.persons, 'Q6093334')
+    Q5967602_pid = index_of_person_id(person_index.persons, 'Q5967602')
+
+    Q6093334_data = person_index.persons.iloc[Q6093334_pid].copy()
+    Q5967602_data = person_index.persons.iloc[Q5967602_pid].copy()
+
+    swap_rows(person_index.persons, Q6093334_pid, Q5967602_pid)
+
+    assert (person_index.persons.iloc[Q6093334_pid].copy() == Q5967602_data).all()
+    assert (person_index.persons.iloc[Q5967602_pid].copy() == Q6093334_data).all()
+
+
+def test_person_with_ambiguous_party():
+    person: md.Person = md.Person(
+        person_id='Q1347810',
+        name='Kilbom',
+        gender_id=1,
+        party_id=0,
+        year_of_birth=1885,
+        year_of_death=1961,
+        terms_of_office=[
+            md.TermOfOffice(office_type_id=1, sub_office_type_id=3, start_year=1922, end_year=1924),
+            md.TermOfOffice(office_type_id=1, sub_office_type_id=3, start_year=1929, end_year=1944),
+        ],
+        alt_parties=[
+            md.PersonParty(party_id=8, start_year=0, end_year=0),
+            md.PersonParty(party_id=1, start_year=0, end_year=0),
+            md.PersonParty(party_id=10, start_year=0, end_year=0),
+        ],
+    )
+
+
+def test_fetch_person():
+    database_filename: str = "./metadata/riksprot_metadata.v0.4.0.db"
+    service = md.SpeekerInfoService(database_filename)
+
+    person: md.Person = service.person_index.get_person('Q1347810')
+    assert person.alt_parties
+    assert len(person.alt_parties) == 3
+    assert set(a.party_id for a in person.alt_parties) == {8, 1, 10}
+    assert set(a.start_year for a in person.alt_parties) == {0}
+    assert set(a.end_year for a in person.alt_parties) == {0}
+    assert person.party_at(1950) == 0
+
+
+def test_speaker_info_repository():
+    database_filename: str = "./metadata/riksprot_metadata.v0.4.0.db"
+    service = md.SpeekerInfoService(database_filename)
+
+    person: md.Person = service.person_index.get_person('Q1347810')
+    assert person
