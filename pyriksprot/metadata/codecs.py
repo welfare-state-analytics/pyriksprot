ec23936febe8d6aeceb001403f0d845fb145c4cc
Roger Mähler <roger.mahler@hotmail.com>
2022-03-07 08:55:00 +0100

    Remved person, sorted code

--- pyriksprot/metadata/codecs.py
+++ pyriksprot/metadata/codecs.py
@@ -4,7 +4,7 @@ import sqlite3
 from contextlib import nullcontext
 from dataclasses import dataclass
 from functools import cached_property
-from typing import Callable, List, Literal, Mapping
+from typing import Callable, Literal, Mapping
 
 import pandas as pd
 
@@ -13,10 +13,10 @@ from . import utility as mdu
 CODE_TABLES: dict[str, str] = {
     'chamber': 'chamber_id',
     'gender': 'gender_id',
-    'office_type': 'office_type_id',
-    'sub_office_type': 'sub_office_type_id',
     'government': 'government_id',
+    'office_type': 'office_type_id',
     'party': 'party_id',
+    'sub_office_type': 'sub_office_type_id',
 }
 
 
@@ -28,24 +28,24 @@ class Codec:
     fx: Callable[[int], str]
     default: str = None
 
+null_frame: pd.DataFrame = pd.DataFrame()
 
 class Codecs:
-    def __init__(self, **kwargs):
-        self.chamber: pd.DataFrame = kwargs.get('chamber')
-        self.gender: pd.DataFrame = kwargs.get('gender')
-        self.office_type: pd.DataFrame = kwargs.get('office_type')
-        self.sub_office_type: pd.DataFrame = kwargs.get('sub_office_type')
-        self.government: pd.DataFrame = kwargs.get('government')
-        self.party: pd.DataFrame = kwargs.get('party')
-        # FIXME: person doesn't belong here
-        self.persons: pd.DataFrame = kwargs.get('persons')
+    def __init__(self):
+        self.chamber: pd.DataFrame = null_frame
+        self.gender: pd.DataFrame = null_frame
+        self.government: pd.DataFrame =null_frame
+        self.office_type: pd.DataFrame = null_frame
+        self.party: pd.DataFrame = null_frame
+        self.sub_office_type: pd.DataFrame = null_frame
+        self.extra_codecs: list[Codec] = []
 
-    @staticmethod
-    def load(source: str | sqlite3.Connection | dict, persons: pd.DataFrame) -> Codecs:
+    def load(self, source: str | sqlite3.Connection | dict) -> Codecs:
         with (sqlite3.connect(database=source) if isinstance(source, str) else nullcontext(source)) as db:
-            code_tables: dict[str, pd.DataFrame] = mdu.load_tables(CODE_TABLES, db=db)
-            code_map: Codecs = Codecs(persons=persons, **code_tables)
-            return code_map
+            tables: dict[str, pd.DataFrame] = mdu.load_tables(CODE_TABLES, db=db)
+            for table_name, table in tables.items():
+                setattr(self, table_name, table)
+        return self
 
     @cached_property
     def gender2name(self) -> dict:
@@ -79,32 +79,17 @@ class Codecs:
     def party_abbrev2id(self) -> dict:
         return mdu.revdict(self.party_abbrev2name)
 
-    @cached_property
-    def pid2person_id(self) -> dict:
-        return self.persons['person_id'].to_dict()
-
-    @cached_property
-    def person_id2pid(self) -> dict:
-        return mdu.revdict(self.pid2person_id)
-
-    @cached_property
-    def pid2person_name(self) -> dict:
-        return self.persons['name'].to_dict()
-
-    @cached_property
+    @property
     def codecs(self) -> list[Codec]:
-        return [
-            Codec('decode', 'office_type_id', 'office_type', self.office_type2name.get),
-            Codec('decode', 'sub_office_type_id', 'sub_office_type', self.sub_office_type2name.get),
+        return self.extra_codecs + [
             Codec('decode', 'gender_id', 'gender', self.gender2name.get),
+            Codec('decode', 'office_type_id', 'office_type', self.office_type2name.get),
             Codec('decode', 'party_id', 'party_abbrev', self.party_abbrev2name.get),
-            Codec('decode', 'pid', 'person_id', self.pid2person_id.get),
-            Codec('encode', 'office_type', 'office_type_id', self.office_type2id.get),
-            Codec('encode', 'sub_office_type', 'sub_office_type_id', self.sub_office_type2id.get),
+            Codec('decode', 'sub_office_type_id', 'sub_office_type', self.sub_office_type2name.get),
             Codec('encode', 'gender', 'gender_id', self.gender2id.get),
+            Codec('encode', 'office_type', 'office_type_id', self.office_type2id.get),
             Codec('encode', 'party', 'party_id', self.party_abbrev2id.get),
-            Codec('encode', 'person_id', 'pid', self.person_id2pid.get),
-            Codec('encode', 'who', 'pid', self.person_id2pid.get),
+            Codec('encode', 'sub_office_type', 'sub_office_type_id', self.sub_office_type2id.get),
         ]
 
     @property
@@ -133,11 +118,10 @@ class Codecs:
         return self.apply_codec(df, self.encoders, drop=drop)
 
     @cached_property
-    def property_values_specs(self) -> List[Mapping[str, str | Mapping[str, int]]]:
+    def property_values_specs(self) -> list[Mapping[str, str | Mapping[str, int]]]:
         return [
             dict(text_name='gender', id_name='gender_id', values=self.gender2id),
             dict(text_name='office_type', id_name='office_type_id', values=self.office_type2id),
-            dict(text_name='sub_office_type', id_name='sub_office_type_id', values=self.sub_office_type2id),
             dict(text_name='party_abbrev', id_name='party_id', values=self.party_abbrev2id),
-            dict(text_name='person_id', id_name='pid', values=self.person_id2pid),
+            dict(text_name='sub_office_type', id_name='sub_office_type_id', values=self.sub_office_type2id),
         ]
