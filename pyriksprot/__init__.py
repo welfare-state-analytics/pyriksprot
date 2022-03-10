e733e06333a4d213fcb46ec87bc17214d15bc1c8
Roger Mähler <roger.mahler@hotmail.com>
2022-03-10 21:05:54 +0100

    Class rename fix

--- pyriksprot/__init__.py
+++ pyriksprot/__init__.py
@@ -4,7 +4,7 @@ from . import metadata, parlaclarin
 from .corpus_index import CorpusSourceIndex, CorpusSourceItem
 from .dehyphenation import SwedishDehyphenator, SwedishDehyphenatorService
 from .interface import ParlaClarinError, Protocol, SegmentLevel, Speech, Utterance
-from .merge import MergedSegmentGroup, SegmentCategoryClosed, SegmentMerger, create_grouping_hashcoder
+from .merge import ProtocolSegmentGroup, SegmentCategoryClosed, SegmentMerger, create_grouping_hashcoder
 from .parlaclarin import compute_term_frequencies, pretokenize
 from .segment import ProtocolSegment, ProtocolSegmentIterator
 from .tag import ITagger, TaggedDocument, tag_protocol, tag_protocol_xml
