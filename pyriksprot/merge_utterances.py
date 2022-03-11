fb4f868d191ec51c4f49a1b969e3c1a969358a11
Roger Mähler <roger.mahler@hotmail.com>
2022-03-11 14:29:59 +0100

    Improved logic

--- pyriksprot/merge_utterances.py
+++ pyriksprot/merge_utterances.py
@@ -12,7 +12,7 @@ from .interface import Protocol, Speech, Utterance
 # pylint: disable=too-many-arguments, no-member
 
 
-class MergeUtteranceStrategyType(str, Enum):
+class MergeStrategyType(str, Enum):
     who = 'who'
     who_sequence = 'who_sequence'
     chain = 'chain'
@@ -21,118 +21,44 @@ class MergeUtteranceStrategyType(str, Enum):
     undefined = 'undefined'
 
 
-def to_speeches(
-    *, protocol: Protocol, merge_strategy: MergeUtteranceStrategyType, segment_skip_size: int = 1, **_
-) -> list[Speech]:
-    """Convert utterances into speeches using specified strategy. Return list."""
-    speeches: list[Speech] = SpeechMergerFactory.get(merge_strategy).speeches(
-        protocol, segment_skip_size=segment_skip_size
-    )
-    return speeches
+def to_speeches(*, protocol: Protocol, merge_strategy: MergeStrategyType, skip_size: int = 1) -> list[Speech]:
 
+    if not protocol.utterances:
+        return []
 
-def group_utterances_by_chain(utterances: list[Utterance]) -> list[list[Utterance]]:
-    """Split utterances based on prev/next pointers. Return list of lists."""
-    speeches: list[list[Utterance]] = []
-    speech: list[Utterance] = []
-    start_of_speech: bool = None
+    merger: IMergeStrategy = MergerFactory.get(merge_strategy)
 
-    for _, u in enumerate(utterances or []):
-        """Rules:
-        - attrib `next` --> start of new speech with consecutive utterances
-        - attrib `prev` --> utterance is belongs to same speech as previous utterance
-        - if neither `prev` or `next` are set, then utterance is the entire speech
-        - attribs `prev` and `next` are never both set
-        """
-
-        if bool(u.prev_id) and bool(u.next_id):
-            raise ValueError(f"logic error: {u.u_id} has both prev/next attrbutes set")
-
-        is_part_of_chain: bool = bool(u.prev_id) or bool(u.next_id)
-        is_unknown_continuation: bool = (
-            bool(speech) and u.who == "unknown" == speech[-1].who and u.speaker_hash == speech[-1].speaker_hash
-        )
-
-        start_of_speech: bool = (
-            True
-            if bool(u.next_id)
-            else not is_unknown_continuation
-            if not is_part_of_chain
-            else not bool(speech) and bool(u.prev_id)
-        )
-
-        if start_of_speech:
-
-            if bool(u.prev_id) and not bool(speech):
-                logger.warning(f"logic error: {u.u_id} has prev attribute but no previous utterance")
-
-            speech = [u]
-            speeches.append(speech)
-
-        else:
-
-            if bool(speech):
-
-                if bool(u.prev_id) and speech[0].u_id != u.prev_id:
-                    logger.warning(f"u[{u.u_id}]: current prev_id differs from first u.u_id '{speech[0].u_id}'")
-
-                if speech[0].who != u.who:
-                    raise ValueError(f"u[{u.u_id}]: multiple who ids in current speech '{speech[0].who}'")
-
-            speech.append(u)
-
-    return speeches
-
-
-def merge_utterances_by_speaker_hash(utterances: list[Utterance]) -> list[list[Utterance]]:
-    """Split utterances based on prev/next pointers. Return list of lists."""
-    speeches: list[list[Utterance]] = [list(g) for _, g in groupby(utterances or [], key=lambda x: x.speaker_hash)]
-    return speeches
-
-
-class IMergeSpeechStrategy(abc.ABC):
-    """Abstract strategy for merging a protocol into speeches"""
-
-    def create(self, protocol: Protocol, utterances: list[Utterance] = None, speech_index: int = 0) -> Speech:
-        """Create a new speech entity."""
-
-        if not utterances:
-            utterances = protocol.utterances
-
-        return Speech(
+    speeches: list[Speech] = [
+        Speech(
             protocol_name=protocol.name,
-            document_name=f'{protocol.name}_{speech_index:03}',
+            document_name=f'{protocol.name}_{(i + 1):03}',
             speech_id=utterances[0].u_id,
             who=utterances[0].who,
             page_number=utterances[0].page_number,
             speech_date=protocol.date,
-            speech_index=speech_index,
+            speech_index=i + 1,
             utterances=utterances,
         )
+        for i, utterances in enumerate(merger.cluster(protocol.utterances))
+    ]
 
-    def speeches(self, protocol: Protocol, segment_skip_size: int = 1) -> list[Speech]:
-        speeches: list[Speech] = self.merge(protocol=protocol)
-        if segment_skip_size > 0:
-            speeches = [s for s in speeches if len(s.text or "") >= segment_skip_size]
-        return speeches
+    if skip_size > 0:
+        speeches = [s for s in speeches if len(s.text or "") >= skip_size]
 
-    def split(self, utterances: Protocol) -> list[list[Utterance]]:  # pylint: disable=unused-argument
-        return []
-
-    def merge(self, protocol: Protocol) -> list[Speech]:
-        """Create a speech for each consecutive sequence with the same `who`. Return list of Speech."""
-        if not protocol.utterances:
-            return []
-        return [
-            self.create(protocol, utterances=utterances, speech_index=i + 1)
-            for i, utterances in enumerate(self.split(protocol.utterances))
-        ]
+    return speeches
 
 
-class MergeSpeechByWho(IMergeSpeechStrategy):
+class IMergeStrategy(abc.ABC):
+    """Abstract strategy for merging a protocol into speeches"""
+
+    def cluster(self, utterances: Protocol) -> list[list[Utterance]]:
+        return [utterances]
+
+
+class MergeByWho(IMergeStrategy):
     """Merge all uterrances for a unique `who` into a single speech """
 
-    def split(self, utterances: Protocol) -> list[list[Utterance]]:
+    def cluster(self, utterances: Protocol) -> list[list[Utterance]]:
         """Create a speech for each unique `who`. Return list of Speech."""
         data = defaultdict(list)
         for u in utterances or []:
@@ -140,61 +66,120 @@ class MergeSpeechByWho(IMergeSpeechStrategy):
         return [data[who] for who in data]
 
 
-class MergeSpeechByWhoSequence(IMergeSpeechStrategy):
+class MergeByWhoSequence(IMergeStrategy):
     """Merge sequences with same `who` into a speech """
 
-    def split(self, utterances: list[Utterance]) -> list[list[Utterance]]:
+    def cluster(self, utterances: list[Utterance]) -> list[list[Utterance]]:
         groups: list[list[Utterance]] = [list(g) for _, g in groupby(utterances or [], key=lambda x: x.who)]
         return groups
 
 
-class MergeSpeechBySpeakerHashSequence(IMergeSpeechStrategy):
+class MergeBySpeakerHashSequence(IMergeStrategy):
     """Merge sequences with same `who` into a speech """
 
-    def split(self, utterances: list[Utterance]) -> list[list[Utterance]]:
+    def cluster(self, utterances: list[Utterance]) -> list[list[Utterance]]:
         groups: list[list[Utterance]] = [list(g) for _, g in groupby(utterances or [], key=lambda x: x.speaker_hash)]
         return groups
 
+    # @staticmethod
+    # def merge_utterances_by_speaker_hash(utterances: list[Utterance]) -> list[list[Utterance]]:
+    #     """Split utterances based on prev/next pointers. Return list of lists."""
+    #     speeches: list[list[Utterance]] = [list(g) for _, g in groupby(utterances or [], key=lambda x: x.speaker_hash)]
+    #     return speeches
 
-class MergeSpeechByWhoSpeakerHashSequence(IMergeSpeechStrategy):
+
+class MergeByWhoSpeakerHashSequence(IMergeStrategy):
     """Merge sequences with same `who` into a speech """
 
-    def split(self, utterances: list[Utterance]) -> list[list[Utterance]]:
+    def cluster(self, utterances: list[Utterance]) -> list[list[Utterance]]:
         groups: list[list[Utterance]] = [
             list(g) for _, g in groupby(utterances or [], key=lambda x: f"{x.who}_{x.speaker_hash}")
         ]
         return groups
 
 
-class MergeSpeechByChain(IMergeSpeechStrategy):
-    def split(self, utterances: list[Utterance]) -> list[list[Utterance]]:
-        groups: list[list[Utterance]] = group_utterances_by_chain(utterances)
+class MergeByChain(IMergeStrategy):
+    def cluster(self, utterances: list[Utterance]) -> list[list[Utterance]]:
+        groups: list[list[Utterance]] = MergeByChain.group_utterances_by_chain(utterances)
         return groups
 
+    @staticmethod
+    def group_utterances_by_chain(utterances: list[Utterance]) -> list[list[Utterance]]:
+        """Split utterances based on prev/next pointers. Return list of lists."""
+        speeches: list[list[Utterance]] = []
+        speech: list[Utterance] = []
+        start_of_speech: bool = None
 
-class UndefinedMergeSpeech(IMergeSpeechStrategy):
-    def split(self, utterances: list[Utterance]) -> list[list[Utterance]]:
+        for _, u in enumerate(utterances or []):
+            """Rules:
+            - attrib `next` --> start of new speech with consecutive utterances
+            - attrib `prev` --> utterance is belongs to same speech as previous utterance
+            - if neither `prev` or `next` are set, then utterance is the entire speech
+            - attribs `prev` and `next` are never both set
+            """
+
+            if bool(u.prev_id) and bool(u.next_id):
+                raise ValueError(f"logic error: {u.u_id} has both prev/next attrbutes set")
+
+            is_part_of_chain: bool = bool(u.prev_id) or bool(u.next_id)
+            is_unknown_continuation: bool = (
+                bool(speech) and u.who == "unknown" == speech[-1].who and u.speaker_hash == speech[-1].speaker_hash
+            )
+
+            start_of_speech: bool = (
+                True
+                if bool(u.next_id)
+                else not is_unknown_continuation
+                if not is_part_of_chain
+                else not bool(speech) and bool(u.prev_id)
+            )
+
+            if start_of_speech:
+
+                if bool(u.prev_id) and not bool(speech):
+                    logger.warning(f"logic error: {u.u_id} has prev attribute but no previous utterance")
+
+                speech = [u]
+                speeches.append(speech)
+
+            else:
+
+                if bool(speech):
+
+                    if bool(u.prev_id) and speech[0].u_id != u.prev_id:
+                        logger.warning(f"u[{u.u_id}]: current prev_id differs from first u.u_id '{speech[0].u_id}'")
+
+                    if speech[0].who != u.who:
+                        raise ValueError(f"u[{u.u_id}]: multiple who ids in current speech '{speech[0].who}'")
+
+                speech.append(u)
+
+        return speeches
+
+
+class UndefinedMerge(IMergeStrategy):
+    def cluster(self, utterances: list[Utterance]) -> list[list[Utterance]]:
         raise ValueError("undefined merge strategy encountered")
 
     def merge(self, protocol: Protocol) -> list[Speech]:
         raise ValueError("undefined merge strategy encountered")
 
 
-class SpeechMergerFactory:
+class MergerFactory:
 
-    strategies: dict[MergeUtteranceStrategyType, IMergeSpeechStrategy] = {
-        'who': MergeSpeechByWho(),
-        'who_sequence': MergeSpeechByWhoSequence(),
-        'who_speaker_hash_sequence': MergeSpeechByWhoSpeakerHashSequence(),
-        'speaker_hash_sequence': MergeSpeechBySpeakerHashSequence(),
-        'chain': MergeSpeechByChain(),
-        'undefined': UndefinedMergeSpeech(),
+    strategies: dict[MergeStrategyType, IMergeStrategy] = {
+        'who': MergeByWho(),
+        'who_sequence': MergeByWhoSequence(),
+        'who_speaker_hash_sequence': MergeByWhoSpeakerHashSequence(),
+        'speaker_hash_sequence': MergeBySpeakerHashSequence(),
+        'chain': MergeByChain(),
+        'undefined': UndefinedMerge(),
     }
 
     @staticmethod
-    def get(strategy: str) -> IMergeSpeechStrategy:
+    def get(strategy: str) -> IMergeStrategy:
         return (
-            SpeechMergerFactory.strategies.get(strategy)
-            if strategy in SpeechMergerFactory.strategies
-            else SpeechMergerFactory.strategies.get('undefined')
+            MergerFactory.strategies.get(strategy)
+            if strategy in MergerFactory.strategies
+            else MergerFactory.strategies.get('undefined')
         )
