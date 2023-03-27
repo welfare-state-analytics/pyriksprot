3b6feccf84a14a5a8300d876de1459ba50398425
roger-mahler <roger.mahler@hotmail.com>
2023-03-27 15:07:31 +0200

    Made method public

--- pyriksprot/workflows/__init__.py
+++ pyriksprot/workflows/__init__.py
@@ -1,4 +1,5 @@
 from .extract_tags import extract_corpus_tags
 from .extract_text import extract_corpus_text
+from .subset_corpus import subset_corpus_and_metadata
 from .tag import tag_protocol, tag_protocol_xml, tag_protocols
 from .tf import compute_term_frequencies
