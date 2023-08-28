05d732ea184463172bc16b650d9ca462f46ccdfe
roger-mahler <roger.mahler@hotmail.com>
2023-08-28 12:00:15 +0200

    Args cleanups and fixes

--- pyriksprot/scripts/riksprot2vrt.py
+++ pyriksprot/scripts/riksprot2vrt.py
@@ -1,8 +1,7 @@
-import os
 import sys
 from glob import glob
-from os.path import basename
-from os.path import join as jj
+from os import makedirs, remove
+from os.path import basename, dirname, exists, isdir, join
 
 import click
 
@@ -10,52 +9,34 @@ from pyriksprot.workflows.export_vrt import VrtExportBatch, export_vrt
 
 
 @click.command()
+@click.option('--source-folder', '-i', type=str, multiple=False, required=True, help="Source folder.")
+@click.option('--target-folder', '-o', type=str, multiple=False, required=True, help="Target folder.")
 @click.option(
-    '--folder-batches',
-    '-i',
-    type=(str, str),
-    multiple=False,
-    required=False,
-    help="Create gzipped batches for all subfolders to batches.",
-)
-@click.option(
-    '--batch', '-b', type=(str, str, str), multiple=True, required=False, help="Batch `source` `target `to export."
-)
-@click.option(
-    '--structural-tags',
+    '--structural-tag',
     '-t',
     type=click.Choice(['protocol', 'speech', 'utterance']),
     multiple=True,
     help="Structural elements to include in the VRT.",
 )
-@click.option('--batch-tag', type=str, default=None, help="Root tag to surround all batches in exported VRT file.")
-@click.option('--batch-date', type=str, default=None, help="Batch tag date attribute.")
 @click.option('--processes', type=int, default=1, help="Number of processes to use.")
 @click.option('--force', is_flag=True, default=False, help="Force overwrite of existing files")
-def main(
-    folder_batches: str = None,
-    batch: list[tuple[str, str, str]] = None,
-    structural_tags: str = None,
-    batch_tag: str = None,
-    batch_date: str = None,
+@click.option('--batch-tag', '-t', type=str, multiple=False, required=False, default='year', help="Batch tag.")
+def export_yearly_folders(
+    source_folder: str,
+    target_folder: str,
+    batch_tag: str = 'year',
+    structural_tag: str = None,
     processes: int = 1,
     force: bool = False,
 ):
     """Export protocols to VRT format."""
     try:
-        if folder_batches and batch:
-            click.echo("Cannot specify both --folder-batches and --batch. Aborting.")
-            sys.exit(1)
-
-        if not folder_batches and not batch:
-            click.echo("Must specify either --folder-batches or --batch. Aborting.")
-            sys.exit(1)
-
-        batches: list[VrtExportBatch] = (
-            get_folder_batches(*folder_batches) if folder_batches else [VrtExportBatch(*b) for b in batch]
+        attribs = {'year': lambda x: int(basename(x)[:4]), 'title': lambda x: basename(x)}
+        batches: list[VrtExportBatch] = create_yearly_folder_batches(
+            source_folder, target_folder, batch_tag=batch_tag, **attribs
         )
 
-        if any(not os.path.exists(b.source) for b in batches):
+        if any(not exists(b.source) for b in batches):
             click.echo("Source folder does not exist. Aborting.")
             sys.exit(1)
 
@@ -63,30 +44,37 @@ def main(
             click.echo("No batches found. Aborting.")
             sys.exit(1)
 
-        os.makedirs(os.path.dirname(batches[0].target), exist_ok=True)
-        existing_targets: list[str] = [b.target for b in batches if os.path.exists(b.target)]
+        makedirs(dirname(batches[0].target), exist_ok=True)
+        existing_targets: list[str] = [b.target for b in batches if exists(b.target)]
         if existing_targets:
             if not force:
                 click.echo(f"Target(s) {existing_targets} already exists. Aborting.")
                 sys.exit(1)
 
             for target in existing_targets:
-                os.remove(target)
+                remove(target)
 
-        export_vrt(batches, *structural_tags, tag=batch_tag, date=batch_date, processes=processes)
+        export_vrt(batches, *structural_tag, processes=processes)
     except Exception as ex:
         click.echo(ex)
         sys.exit(1)
 
 
-def get_folder_batches(source_folder: str, target_folder: str) -> list[VrtExportBatch]:
+def create_yearly_folder_batches(
+    source_folder: str, target_folder: str, batch_tag: str = None, **attribs
+) -> list[VrtExportBatch]:
     """Get all subfolders in source folder and create a batch for each."""
     return [
-        VrtExportBatch(basename(folder), folder, jj(target_folder, f"{basename(folder)}.vrt.gz"))
-        for folder in glob(jj(source_folder, "*"))
-        if os.path.isdir(folder)
+        VrtExportBatch(
+            folder,
+            join(target_folder, f"{basename(folder)}.vrt.gz"),
+            batch_tag,
+            {attr: fx(folder) for attr, fx in attribs.items()},
+        )
+        for folder in glob(join(source_folder, "*"))
+        if isdir(folder) and basename(folder).isnumeric()
     ]
 
 
 if __name__ == "__main__":
-    main()
+    export_yearly_folders()
