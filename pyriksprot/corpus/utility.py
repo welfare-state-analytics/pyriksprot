3683c698725ccf6a664e5464442242746253ccfc
roger-mahler <roger.mahler@hotmail.com>
2024-09-10 16:25:48 +0200

    change: added better list corpus files functions

--- pyriksprot/corpus/utility.py
+++ pyriksprot/corpus/utility.py
@@ -1,8 +1,15 @@
+import fnmatch
 import glob
 import os
+import re
 import shutil
-from os.path import expanduser
+import xml
+import xml.etree.ElementTree as ET
+from calendar import c
+from collections import defaultdict
+from os.path import abspath, basename, expanduser
 from os.path import join as jj
+from typing import Any, Generator, Literal
 from urllib.parse import quote as q
 from urllib.request import urlretrieve
 
@@ -10,6 +17,58 @@ from loguru import logger
 
 from .. import gitchen as gh
 from ..utility import ensure_folder, ensure_path, replace_extension, reset_folder
+
+
+def _extract_tei_corpus_filenames(tei_filename: str) -> list[str]:
+    """Extracts filenames from a TEI Corpus file."""
+    # root: ET.Element = ET.fromstring(source)
+    tree: ET.ElementTree = ET.parse(tei_filename)
+    root = tree.getroot()
+    namespaces: dict[str, str] = {'xi': 'http://www.w3.org/2001/XInclude'}
+    filenames: list[str] = [
+        element.get('href') for element in root.findall('.//xi:include', namespaces) if element.get('href') is not None
+    ]
+    return filenames
+
+
+def ls_corpus_by_tei_corpora(
+    folder: str, mode: Literal['dict', 'filenames', 'tuples'] = 'dict', normalize: bool = True
+) -> dict[str, dict[str, str | list[str]]] | list[str] | list[tuple[str, str]]:
+    """Returns a list of XML documents as specified in teiCorpus files that reside in `folder`."""
+    tei_corpora_filenames: list[str] = glob.glob(jj(folder, 'prot-*.xml'), recursive=False)
+    data: dict[str, dict[str, str | list[str]]] = {}
+    for tei_filename in tei_corpora_filenames:
+        chamber: str = basename(tei_filename).split('-')[1].split('.')[0]
+        if chamber not in ['ak', 'fk', 'ek']:
+            raise ValueError(f"illegal chamber: {chamber}")
+        filenames: str = [replace_extension(f, 'xml') for f in _extract_tei_corpus_filenames(tei_filename)]
+        if normalize:
+            filenames = [abspath(jj(folder, filename)) for filename in filenames]
+        data[chamber] = {
+            'chamber': chamber,
+            'filenames': filenames,
+        }
+    if mode == 'dict':
+        return data
+    elif mode == 'filenames':
+        return [filename for item in data.values() for filename in item['filenames']]
+    elif mode == 'tuples':
+        return [(item['chamber'], filename) for item in data.values() for filename in item['filenames']]
+    return data
+
+
+def ls_corpus_folder(folder: str, pattern: str = None) -> list[str]:
+    """List all ParlaCLARIN files in a folder and it's subfolders."""
+
+    if pattern is not None:
+        if pattern.startswith('**/'):
+            pattern = pattern[3:]
+        return glob.glob(jj(folder, '**', pattern), recursive=True)
+
+    regex_pattern: str = r'prot-\d*-.*-\d*\.xml'
+    rx: re.Pattern[str] = re.compile(pattern)
+    candidates: list[str] = glob.glob(jj(folder, '**', 'prot-*.xml'), recursive=True)
+    return [f for f in candidates if rx.match(basename(f))]
 
 
 def _download_to_folder(*, url: str, target_folder: str, filename: str) -> None:
