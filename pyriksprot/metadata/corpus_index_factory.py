87cf141ece20b223fc1a5571bd33bd753edb6ab1
roger-mahler <roger.mahler@hotmail.com>
2024-09-12 21:06:19 +0200

    refactor: corpus index generator

--- pyriksprot/metadata/corpus_index_factory.py
+++ pyriksprot/metadata/corpus_index_factory.py
@@ -1,19 +1,135 @@
 from __future__ import annotations
 
+import itertools
 import os
-from typing import Type
+from types import SimpleNamespace
+from typing import Sequence, Type
 
+from dataclasses import dataclass, field
+from unittest import result
 import pandas as pd
 from loguru import logger
 from tqdm import tqdm
 
 from pyriksprot.corpus.utility import ls_corpus_folder
-from pyriksprot.interface import IProtocol, IProtocolParser, SpeakerNote
+from pyriksprot.interface import MISSING_SPEAKER_NOTE, IProtocol, IProtocolParser, SpeakerNote
 
 from . import database
 from .schema import MetadataSchema
 
 jj = os.path.join
+
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from tqdm import tqdm
+from dataclasses import dataclass
+from typing import Sequence, Type
+
+class CorpusScanner:
+
+    @dataclass
+    class ScanResult:
+        protocols: list[tuple[int, str]] = field(default_factory=list)
+        utterances: list[tuple] = field(default_factory=list)
+        page_references: list[tuple] = field(default_factory=list)
+        speaker_notes: dict[str, SpeakerNote] = field(default_factory=dict)
+
+    def __init__(self, parser: IProtocolParser | Type[IProtocolParser]) -> None:
+        self.parser: IProtocolParser | Type[IProtocolParser] = parser
+
+    def scan(self, filenames: Sequence[str]) -> CorpusScanner.ScanResult:
+
+        data: CorpusScanner.ScanResult = CorpusScanner.ScanResult()
+
+        for document_id, filename in tqdm(enumerate(filenames)):
+            protocol: IProtocol = self.parser.parse(filename, ignore_tags={"teiHeader"})
+            data.protocols.append((document_id, protocol.name, protocol.date, int(protocol.date[:4])))
+            data.utterances.extend(
+                tuple([document_id, u.u_id, u.who, u.speaker_note_id, u.page_number]) for u in protocol.utterances
+            )
+            data.page_references.extend(
+                tuple([document_id, p.source_id, p.page_number, p.reference]) for p in protocol.page_references
+            )
+
+            data.speaker_notes.update(protocol.get_speaker_notes())
+
+        return data
+
+    # def parallel_scan(self, filenames: Sequence[str]) -> CorpusScanner.ScanResult:
+    #     data: CorpusScanner.ScanResult = CorpusScanner.ScanResult()
+
+    #     # Function to process a single file
+    #     def process_file(document_id, filename):
+    #         protocol: IProtocol = self.parser.parse(filename, ignore_tags={"teiHeader"})
+    #         protocols = (document_id, protocol.name, protocol.date, int(protocol.date[:4])) 
+    #         utterances = [
+    #             (document_id, u.u_id, u.who, u.speaker_note_id, u.page_number) for u in protocol.utterances
+    #         ]
+    #         page_references = [
+    #             (document_id, p.source_id, p.page_number, p.reference) for p in protocol.page_references
+    #         ]
+    #         speaker_notes = protocol.get_speaker_notes()
+    #         return protocols, utterances, page_references, speaker_notes
+
+    #     # Parallel processing with ThreadPoolExecutor
+    #     with ThreadPoolExecutor() as executor:
+    #         futures = {
+    #             executor.submit(process_file, document_id, filename): filename 
+    #             for document_id, filename in enumerate(filenames)
+    #         }
+
+    #         # Process each future as it completes
+    #         for future in tqdm(as_completed(futures), total=len(futures)):
+    #             protocols, utterances, page_references, speaker_notes = future.result()
+
+    #             # Append and extend data to the main ScanResult object
+    #             data.protocols.append(protocols)
+    #             data.utterances.extend(utterances)
+    #             data.page_references.extend(page_references)
+    #             data.speaker_notes.update(speaker_notes)
+
+    #     return data
+
+    def to_dataframes(self, result: CorpusScanner.ScanResult) -> dict[str, pd.DataFrame]:
+        """Store enough source reference data to reconstruct source urls."""
+
+        protocols: pd.DataFrame = pd.DataFrame(
+            data=result.protocols, columns=['document_id', 'document_name', 'date', 'year']
+        ).set_index("document_id")
+
+        utterances: pd.DataFrame = pd.DataFrame(
+            data=result.utterances,
+            columns=['document_id', 'u_id', 'person_id', 'speaker_note_id', 'page_number'],
+        ).set_index("u_id")
+
+        page_references: pd.DataFrame = pd.DataFrame(
+            data=result.page_references, columns=['document_id', 'source_id', 'page_number', 'reference']
+        ).set_index(['document_id', 'page_number'], drop=True)
+
+        source_references: pd.DataFrame = (
+            page_references[page_references.reference != ""]  # pylint: disable=unsubscriptable-object
+            .reset_index()[['document_id', 'source_id', 'reference']]
+            .copy()
+            .drop_duplicates()
+        ).set_index(['document_id'], drop=True)
+        page_references.drop(columns=['reference'], inplace=True)
+
+        missing: tuple[str, str] = (MISSING_SPEAKER_NOTE.speaker_note_id, MISSING_SPEAKER_NOTE.speaker_note)
+        speaker_notes_data: pd.DataFrame = pd.DataFrame(
+            itertools.chain([missing], ((x.speaker_note_id, x.speaker_note) for x in result.speaker_notes.values())),
+            columns=['speaker_note_id', 'speaker_note'],
+        ).set_index('speaker_note_id')
+
+        protocol_ids: set[int] = set(utterances.document_id.unique())
+        empty_protocols: pd.DataFrame = protocols[~protocols.index.isin(protocol_ids)]
+
+        return {
+            "protocols": protocols,
+            "utterances": utterances,
+            "page_references": page_references,
+            "source_references": source_references,
+            "speaker_notes": speaker_notes_data,
+            "empty_protocols": empty_protocols,
+        }
 
 
 class CorpusIndexFactory:
@@ -33,62 +149,10 @@ class CorpusIndexFactory:
 
         return self.collect(filenames).to_csv(target_folder)
 
-    def _empty(self) -> pd.DataFrame:
-        """Find and store protocols without any utterance."""
-        protocols: pd.DataFrame = self.data["protocols"]
-        protocol_ids: set[int] = set(self.data["utterances"].document_id.unique())
-        return protocols[~protocols.index.isin(protocol_ids)]
-
     def collect(self, filenames) -> CorpusIndexFactory:
-        utterance_data: list[tuple] = []
-        page_reference_data: list[tuple] = []
-        protocol_data: list[tuple[int, str]] = []
-        speaker_notes: dict[str, SpeakerNote] = {}
 
-        for document_id, filename in tqdm(enumerate(filenames)):
-            protocol: IProtocol = self.parser.parse(filename, ignore_tags={"teiHeader"})
-            protocol_data.append((document_id, protocol.name, protocol.date, int(protocol.date[:4])))
-            utterance_data.extend(
-                tuple([document_id, u.u_id, u.who, u.speaker_note_id, u.page_number]) for u in protocol.utterances
-            )
-            page_reference_data.extend(
-                tuple([document_id, p.source_id, p.page_number, p.reference]) for p in protocol.page_references
-            )
-
-            speaker_notes.update(protocol.get_speaker_notes())
-
-        """Store enough source reference data to reconstruct source urls."""
-        page_references: pd.DataFrame = pd.DataFrame(
-            data=page_reference_data, columns=['document_id', 'source_id', 'page_number', 'reference']
-        ).set_index(['document_id', 'page_number'], drop=True)
-
-        source_references: pd.DataFrame = (
-            page_references[page_references.reference != ""]  # pylint: disable=unsubscriptable-object
-            .reset_index()[['document_id', 'source_id', 'reference']]
-            .copy()
-            .drop_duplicates()
-        ).set_index(['document_id'], drop=True)
-        page_references.drop(columns=['reference'], inplace=True)
-
-        speaker_notes_data: pd.DataFrame = pd.DataFrame(
-            ((x.speaker_note_id, x.speaker_note) for x in speaker_notes.values()),
-            columns=['speaker_note_id', 'speaker_note'],
-        ).set_index('speaker_note_id')
-
-        self.data = {
-            "protocols": pd.DataFrame(
-                data=protocol_data, columns=['document_id', 'document_name', 'date', 'year']
-            ).set_index("document_id"),
-            "utterances": pd.DataFrame(
-                data=utterance_data,
-                columns=['document_id', 'u_id', 'person_id', 'speaker_note_id', 'page_number'],
-            ).set_index("u_id"),
-            "page_references": page_references,
-            'source_references': source_references,
-            "speaker_notes": speaker_notes_data,
-        }
-        self.data['empty_protocols'] = self._empty()
-
+        service: CorpusScanner = CorpusScanner(self.parser)
+        self.data = service.to_dataframes(service.scan(filenames))
         return self
 
     def to_csv(self, folder: str) -> CorpusIndexFactory:
