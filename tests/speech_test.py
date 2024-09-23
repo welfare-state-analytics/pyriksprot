c8cd4d2f41d0ce361354f7343938ebcdec2aac04
roger-mahler <roger.mahler@hotmail.com>
2024-09-23 16:17:46 +0200

    tests: adjusted expected values

--- tests/speech_test.py
+++ tests/speech_test.py
@@ -145,8 +145,8 @@ Hence it is (for now) commented out.
         ("prot-1955--ak--022.xml", 163, ts.MergeStrategyType.chain),
         ("prot-1955--ak--022.xml", 163, ts.MergeStrategyType.chain_consecutive_unknowns),
         ("prot-1955--ak--022.xml", 160, ts.MergeStrategyType.who_sequence),
-        ('prot-199192--127.xml', 248, ts.MergeStrategyType.chain),
-        ('prot-199192--127.xml', 248, ts.MergeStrategyType.chain_consecutive_unknowns),
+        ('prot-199192--127.xml', 249, ts.MergeStrategyType.chain),
+        ('prot-199192--127.xml', 249, ts.MergeStrategyType.chain_consecutive_unknowns),
         ('prot-199192--127.xml', 54, ts.MergeStrategyType.who),
         ('prot-199192--127.xml', 248, ts.MergeStrategyType.who_sequence),
     ],
@@ -274,7 +274,7 @@ def test_load_protocols_from_folder():
         ('prot-1955--ak--022', 'who_sequence', 160),
         ('prot-1955--ak--022', 'chain', 163),
         ('prot-199192--127', 'who_sequence', 248),
-        ('prot-199192--127', 'chain', 248),
+        ('prot-199192--127', 'chain', 249),
     ],
 )
 def test_protocol_to_items(protocol_name: str, merge_strategy: str, expected_speech_count: int):
