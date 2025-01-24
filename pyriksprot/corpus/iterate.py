5c25b88670d4597b548f12fa9a7990df31e1aa14
roger-mahler <roger.mahler@hotmail.com>
2025-01-24 14:45:42 +0100

    fix: remove unused import from iterate.py

--- pyriksprot/corpus/iterate.py
+++ pyriksprot/corpus/iterate.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import abc
-from collections.abc import Iterator
 from dataclasses import dataclass
 from multiprocessing import get_context
 from typing import TYPE_CHECKING, Callable, Iterable, Literal
