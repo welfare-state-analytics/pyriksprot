22bbfbdf59f94036fd1c6734171656b8bbebd952
roger-mahler <roger.mahler@hotmail.com>
2025-01-24 14:47:01 +0100

    fix: update type hint for compose function and improve variable naming in utility.py

--- pyriksprot/utility.py
+++ pyriksprot/utility.py
@@ -185,7 +185,7 @@ def replace_extension(filename: str, extension: str) -> str:
 
 
 def path_add_suffix(path: str, suffix: str, new_extension: str = None) -> str:
-    name, _ = splitext(path)
+    name, extension = splitext(path) # type: ignore
     return f'{name}{suffix}{extension if new_extension is None else new_extension}'
 
 
@@ -411,11 +411,11 @@ def slugify(value: str, allow_unicode: bool = False) -> str:
     return re.sub(r'[-\s]+', '-', value).strip('-_')
 
 
-def compose(*fns: Sequence[Callable[[str], str]]) -> Callable[[str], str]:
+def compose(*fns: Sequence[Callable[[str], str]]) -> None | Callable[[str], str]:
     """Create a composed function from a list of function. Return function."""
     if len(fns) == 0:
         return None
-    return functools.reduce(lambda f, g: lambda *args: f(g(*args)), fns)
+    return functools.reduce(lambda f, g: lambda *args: f(g(*args)), fns) # type: ignore
 
 
 def is_empty(filename: str) -> bool:
